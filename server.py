3db85df88df5f0466caa8746b9ab41cdf853da65
Mateusz Trzeciak <trzeciakmat@student.agh.edu.pl>
2023-11-20 12:27:47 +0100

    Improved server.py

--- server.py
+++ server.py
@@ -1,13 +1,31 @@
-# write function which will listen udp segment on port 8765 and print the data
-# received on the terminal
-# Use python3
-# Hint: use socket library
-
 import socket
+import binascii
+import os
+import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('192.168.43.170' , 8765))
+UDP_IP = ""
+UDP_PORT = 8765
+UDP_BUFFER = 32768
+SAVE_FOLDER = "received_data/"
+
+# Create the save folder if it doesn't exist
+if not os.path.exists(SAVE_FOLDER):
+    os.makedirs(SAVE_FOLDER)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
 while True:
-    data, addr = s.recvfrom(1024*20)
-    print("received message: %s" % data)
-    print("from: %s" % addr)
+    data, addr = sock.recvfrom(UDP_BUFFER)
+
+    # Convert the data to a hex dump
+    hex_dump = binascii.hexlify(data).decode('utf-8')
+
+    # Save the hex dump to a text file
+    timestamp = time.time()
+    file_path = os.path.join(SAVE_FOLDER, f"received_hexdump_{timestamp}.txt")
+
+    with open(file_path, 'w') as file:
+        file.write(hex_dump)
+
+    print(f"Received and saved hex dump to: {file_path}")
