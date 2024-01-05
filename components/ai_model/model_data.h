6a1fb252e638cb55c71d1b24bd6f6b956c5c244d
Mateusz Trzeciak <trzeciakmat@student.agh.edu.pl>
2024-01-05 11:05:26 +0100

    added ai_model files

---
+++ components/ai_model/model_data.h
@@ -0,0 +1,7 @@
+#ifndef ESP32_CAM_SCANNER_MODEL_DATA_H
+#define ESP32_CAM_SCANNER_MODEL_DATA_H
+
+extern const unsigned char hned_tflite[];
+extern const unsigned int hned_tflite_len;
+
+#endif //ESP32_CAM_SCANNER_MODEL_DATA_H
