86db129340725bc114eb3bf9c3d327b5b8350af9
Mateusz Trzeciak <trzeciakmat@student.agh.edu.pl>
2024-01-06 13:06:17 +0100

    Added comments describing some issues

--- main/websocket_manager.h
+++ main/websocket_manager.h
@@ -4,7 +4,13 @@
 #include <esp_http_client.h>
 #include "esp_camera.h"
 
-void websocket_init(void);
-void websocket_send(void *pvParameters);
-
+// prepare C functions to be called from C++ code
+#ifdef __cplusplus
+extern "C" {
+#endif
+    void websocket_init(void);
+    void websocket_send(void *pvParameters);
+#ifdef __cplusplus
+}
+#endif
 #endif //ESP32_CAM_SCANNER_WEBSOCKET_MANAGER_H
